f662b808167c82460d98d74a2f353651c4433e78
Prasopchok Wattanaolan <psc.tt8@gmail.com>
2019-08-28 23:25:08 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,6 @@ import { HomeComponent } from './components/home/home.component';
 import { GchordsComponent } from './components/gchords/gchords.component';
 import { BchordsComponent } from './components/bchords/bchords.component';
 import { PchordsComponent } from './components/pchords/pchords.component';
-import { AComponent } from './components/gchords/A/A.component';
-import { BComponent } from './components/gchords/B/B.component';
-import { B1Component } from './components/gchords/B1/B1.component';
-import { CComponent } from './components/gchords/C/C.component';
-import { C1Component } from './components/gchords/C1/C1.component';
-import { DComponent } from './components/gchords/D/D.component';
-import { EComponent } from './components/gchords/E/E.component';
-import { E1Component } from './components/gchords/E1/E1.component';
-import { FComponent } from './components/gchords/F/F.component';
-import { F1Component } from './components/gchords/F1/F1.component';
-import { GComponent } from './components/gchords/G/G.component';
-import { G1Component } from './components/gchords/G1/G1.component';
 
 const routes: Routes = [
   {path: '',component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,6 @@ import { FooterComponent } from './components/footer/footer.component';
 import { GchordsComponent } from './components/gchords/gchords.component';
 import { BchordsComponent } from './components/bchords/bchords.component';
 import { PchordsComponent } from './components/pchords/pchords.component';
-import { AComponent } from './components/gchords/a/a.component';
-import { BComponent } from './components/gchords/b/b.component';
-import { B1Component } from './components/gchords/b1/b1.component';
-import { CComponent } from './components/gchords/c/c.component';
-import { C1Component } from './components/gchords/c1/c1.component';
-import { DComponent } from './components/gchords/d/d.component';
-import { EComponent } from './components/gchords/e/e.component';
-import { E1Component } from './components/gchords/e1/e1.component';
-import { FComponent } from './components/gchords/f/f.component';
-import { F1Component } from './components/gchords/f1/f1.component';
-import { GComponent } from './components/gchords/g/g.component';
-import { G1Component } from './components/gchords/g1/g1.component';
 
 @NgModule({
   declarations: [
