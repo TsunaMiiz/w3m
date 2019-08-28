b9f745364455950b9f2bd0079a77bbab4b052455
Prasopchok Wattanaolan <psc.tt8@gmail.com>
2019-08-28 23:37:11 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AComponent } from './components/gchords/a/a.component';
+import { B1Component } from './components/gchords/b1/b1.component';
+import { BComponent } from './components/gchords/b/b.component';
+import { CComponent } from './components/gchords/c/c.component';
+import { C1Component } from './components/gchords/c1/c1.component';
+import { DComponent } from './components/gchords/d/d.component';
+import { E1Component } from './components/gchords/e1/e1.component';
+import { EComponent } from './components/gchords/e/e.component';
+import { FComponent } from './components/gchords/f/f.component';
+import { F1Component } from './components/gchords/f1/f1.component';
+import { GComponent } from './components/gchords/g/g.component';
+import { G1Component } from './components/gchords/g1/g1.component';
 import { GchordsComponent } from './components/gchords/gchords.component';
 import { BchordsComponent } from './components/bchords/bchords.component';
 import { PchordsComponent } from './components/pchords/pchords.component';
@@ -10,6 +22,18 @@ const routes: Routes = [
   {path: 'gchords',component: GchordsComponent},
   {path: 'bchords',component: BchordsComponent},
   {path: 'pchords',component: PchordsComponent},
+  {path: 'gchords/a', component: AComponent},
+  {path: 'gchords/bb', component: B1Component},
+  {path: 'gchords/b', component: BComponent},
+  {path: 'gchords/c', component: CComponent},
+  {path: 'gchords/c#', component: C1Component},
+  {path: 'gchords/d', component: DComponent},
+  {path: 'gchords/eb', component: E1Component},
+  {path: 'gchords/e', component: EComponent},
+  {path: 'gchords/f', component: FComponent},
+  {path: 'gchords/f#', component: F1Component},
+  {path: 'gchords/g', component: GComponent},
+  {path: 'gchords/g#', component: G1Component},
 
 ];
 
@@ -18,3 +42,10 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [HomeComponent,
+                                 GchordsComponent,
+                                 BchordsComponent,
+                                 PchordsComponent,
+                                 AComponent,
+                                 B1Component,
+                                 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,6 +9,18 @@ import { FooterComponent } from './components/footer/footer.component';
 import { GchordsComponent } from './components/gchords/gchords.component';
 import { BchordsComponent } from './components/bchords/bchords.component';
 import { PchordsComponent } from './components/pchords/pchords.component';
+import { AComponent } from './components/gchords/a/a.component';
+import { BComponent } from './components/gchords/b/b.component';
+import { B1Component } from './components/gchords/b1/b1.component';
+import { CComponent } from './components/gchords/c/c.component';
+import { C1Component } from './components/gchords/c1/c1.component';
+import { DComponent } from './components/gchords/d/d.component';
+import { EComponent } from './components/gchords/e/e.component';
+import { E1Component } from './components/gchords/e1/e1.component';
+import { FComponent } from './components/gchords/f/f.component';
+import { F1Component } from './components/gchords/f1/f1.component';
+import { GComponent } from './components/gchords/g/g.component';
+import { G1Component } from './components/gchords/g1/g1.component';
 
 @NgModule({
   declarations: [
